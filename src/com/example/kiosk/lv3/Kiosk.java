c11c2748440c3344ed44115e82211b747e20bbd1
juyoung <dlaks154@gmail.com>
2024-11-25 16:34:56 +0900

    docs

--- src/com/example/kiosk/lv3/Kiosk.java
+++ src/com/example/kiosk/lv3/Kiosk.java
@@ -1,4 +1,54 @@
 package com.example.kiosk.lv3;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Kiosk {
+    private List<MenuItem> menuItems;
+
+    public Kiosk(List<MenuItem> menuItem){
+        this.menuItems = menuItem;
+    }
+
+    public void start(){
+        Scanner sc = new Scanner(System.in);
+        boolean running = true;
+
+        while (running) {
+            // 메뉴 출력
+            System.out.println("\n햄버거 메뉴:");
+            for (int i = 0; i < menuItems.size(); i++) {
+                System.out.println((i + 1) + ". " + menuItems.get(i).getName());
+            }
+            System.out.println("0. 프로그램 종료");
+
+            // 사용자 입력 받기
+            System.out.print("메뉴를 선택하세요 (번호 입력): ");
+            int choice;
+
+            try {
+                choice = Integer.parseInt(sc.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("잘못된 입력입니다. 다시 선택해주세요.");
+                continue;
+            }
+
+            // switch문으로 처리
+            switch (choice) {
+                case 0:
+                    System.out.println("프로그램을 종료합니다.");
+                    running = false;
+                    break;
+                default:
+                    if(choice > 0 && choice <= menuItems.size()) {
+                        MenuItem selectedItem = menuItems.get(choice - 1);
+                        System.out.println("\n선택한 메뉴:");
+                        System.out.println(choice + ". " + selectedItem.getName()+" | W "+selectedItem.getPrice()+" | "+selectedItem.getDescription());
+                    }
+                    break;
+            }
+        }
+        // Scanner 닫기
+        sc.close();
+    }
 }
--- src/com/example/kiosk/lv3/MenuItem.java
+++ src/com/example/kiosk/lv3/MenuItem.java
@@ -1,4 +1,28 @@
 package com.example.kiosk.lv3;
 
 public class MenuItem {
+    private String name; // 이름
+    private double price;  // 가격
+    private String description; // 설명
+
+    public MenuItem(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    // 이름 가져오기
+    public String getName(){
+        return name;
+    }
+
+    // 가격 가져오기
+    public double getPrice(){
+        return price;
+    }
+
+    // 설명 가져오기
+    public String getDescription() {
+        return description;
+    }
 }
